5d470a1e0c235ed5b50e30e552806ddbe13891a7
mrtnvgr <avoiding-commit-stats@example.com>
2025-11-01 17:45:46 +0700

    commit.py: add

--- commit.py
+++ commit.py
@@ -3,6 +3,10 @@ from git import Repo
 from pathlib import Path
 import sys, os
 
+def say(*args, **kwargs):
+    print(*args, **kwargs)
+    sys.stdout.flush()
+
 repo = Repo.init(".")
 tree = repo.head.commit.tree
 
@@ -14,19 +18,20 @@ files = list(map(str, Path(".").rglob("*")))
 is_file = lambda x: not os.path.isdir(x)
 files = list(filter(is_file, files))
 
-is_new = lambda x: x not in tree
+is_new = lambda x: x in tree
 files = list(filter(is_new, files))
+
+skip_git = lambda x: not x.startswith(".git")
+files = list(filter(skip_git, files))
 
 count = len(files)
 print(f"Found {count} new file(s)")
 
 for i, file in enumerate(files):
-    print(f"[{i+1}/{count}] {file}......", end="")
-    sys.stdout.flush()
+    say(f"[{i+1}/{count}] {file}......", end="")
 
     repo.index.add(file)
     repo.index.commit(f"{file}: add", author = fake_me)
     origin.push()
 
-    print("Done!")
-    sys.stdout.flush()
+    say("Done!")
